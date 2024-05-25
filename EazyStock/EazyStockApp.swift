568cadeed9cb30db7e38ef51243b4cdbb8a43803
Filipe Marques <filipe.marques@compasso.com.br>
2024-05-25 13:00:42 -0300

    add my project to repo

---
+++ EazyStock/EazyStockApp.swift
@@ -0,0 +1,17 @@
+//
+//  EazyStockApp.swift
+//  EazyStock
+//
+//  Created by Filipe Camargo Marques on 25/05/24.
+//
+
+import SwiftUI
+
+@main
+struct EazyStockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
